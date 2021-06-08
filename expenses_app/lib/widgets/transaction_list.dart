49cb94f4e94e733dd7f9408d2bb1278fcbdfe989
Muhammad Ghozi <ghozi.mghozi@gmail.com>
2021-06-08 14:45:31 +0700

    update transaction

---
+++ expenses_app/lib/widgets/transaction_list.dart
@@ -0,0 +1,14 @@
+import 'package:expenses_app/widgets/expense_item.dart';
+import 'package:flutter/material.dart';
+import '../models/transaction.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+
+  TransactionList(this.transactions)
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: transactions.map((item) => ExpenseItem(transaction: item)).toList());
+  }
+}
