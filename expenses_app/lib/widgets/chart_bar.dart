34e7c8bb27f8181116f58e365ca9845f191bf121
MuGhz <ghozi.mghozi@gmail.com>
2021-06-23 12:04:38 +0700

    add layout builder

--- expenses_app/lib/widgets/chart_bar.dart
+++ expenses_app/lib/widgets/chart_bar.dart
@@ -10,44 +10,48 @@ class ChartBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     print('name $name amount $amount total $total');
-    return Column(children: <Widget>[
-      Container(
-          height: 20,
-          child: FittedBox(child: Text('\$${amount.toStringAsFixed(0)}'))),
-      SizedBox(
-        height: 4,
-      ),
-      Container(
-          height: 56,
-          width: 10,
-          child: Stack(
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.grey,
-                      width: 1.0,
-                    ),
-                    color: Color.fromRGBO(220, 220, 220, 1),
-                    borderRadius: BorderRadius.circular(8)),
-              ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: FractionallySizedBox(
-                  heightFactor: total,
-                  child: Container(
+    return LayoutBuilder(
+      builder: (context, constraint) {
+        return Column(children: <Widget>[
+          Container(
+              height: constraint.maxHeight * 0.15,
+              child: FittedBox(child: Text('\$${amount.toStringAsFixed(0)}'))),
+          SizedBox(
+            height: constraint.maxHeight * 0.05,
+          ),
+          Container(
+              height: constraint.maxHeight * 0.6,
+              width: 10,
+              child: Stack(
+                children: <Widget>[
+                  Container(
                     decoration: BoxDecoration(
-                        color: Theme.of(context).primaryColor,
+                        border: Border.all(
+                          color: Colors.grey,
+                          width: 1.0,
+                        ),
+                        color: Color.fromRGBO(220, 220, 220, 1),
                         borderRadius: BorderRadius.circular(8)),
                   ),
-                ),
-              ),
-              SizedBox(
-                height: 4,
-              ),
-            ],
-          )),
-      Text(name)
-    ]);
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: FractionallySizedBox(
+                      heightFactor: total,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Theme.of(context).primaryColor,
+                            borderRadius: BorderRadius.circular(8)),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: constraint.maxHeight * 0.05,
+                  ),
+                ],
+              )),
+          Container(height: constraint.maxHeight * 0.15, child: Text(name))
+        ]);
+      },
+    );
   }
 }
