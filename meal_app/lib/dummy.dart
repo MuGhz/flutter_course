6ef8f5829a813c5531c57eac189f3e536b0a3e84
Macbook pro <macbookpro@Macbooks-MacBook-Pro.local>
2021-07-08 10:53:48 +0700

    add model and categories screen

---
+++ meal_app/lib/dummy.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import './models/category.dart';
+const DUMYY_CATEGORIES = const[
+Category(id: '1',title: 'Italian'),
+Category(id: '2',title: 'Japanese',color: Colors.red),
+Category(id: '3',title: 'Indonesian',color: Colors.blue),
+];
